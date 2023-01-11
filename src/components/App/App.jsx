b68ce6e36b76806bf900ac2798914c707c9c2081
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2023-01-11 07:51:50 +0200

    Update App.jsx

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -2,10 +2,10 @@ import { Profile } from '../Profile/Profile';
 import { Statistics } from '../Statistics/Statistics';
 import { FriendList } from '../FriendList/FriendList';
 import {TransactionHistory} from '../TransactionHistory/TransactionHistory';
-import user from './Profile/user.json';
-import data from './Statistics/data.json';
-import friends from './FriendList/friends.json';
-import transactions from './TransactionHistory/transactions.json';
+import user from '../Profile/user.json';
+import data from '../Statistics/data.json';
+import friends from '../FriendList/friends.json';
+import transactions from '../TransactionHistory/transactions.json';
 import {AppContainer} from './App.styled';
 
 export const App = () => {
