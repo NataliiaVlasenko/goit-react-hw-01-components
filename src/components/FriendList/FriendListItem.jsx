562b5ae86809f43fb053b003880c684ae0998ac1
NataliiaVlasenko <108278401+NataliiaVlasenko@users.noreply.github.com>
2023-01-11 07:48:40 +0200

    fix props

--- src/components/FriendList/FriendListItem.jsx
+++ src/components/FriendList/FriendListItem.jsx
@@ -1,26 +1,11 @@
-
-import PropTypes from 'prop-types';
-import { FriendItem,FriendName,FriendOnlineStatus } from './Friends.styled';
-export const FriendListItem = ({friend:{avatar,name,isOnline}}) => {
-    
-return(
-    
+import { FriendItem, FriendName, FriendOnlineStatus } from './Friends.styled';
+export const FriendListItem = ({ friend: { avatar, name, isOnline } }) => {
+  return (
     <FriendItem>
-     
-  <FriendOnlineStatus>{isOnline}</FriendOnlineStatus>
-  
-  <img  src={avatar} alt="User avatar" width="48" />
-  <FriendName>{name}</FriendName>
-</FriendItem>
-)
+      <FriendOnlineStatus>{isOnline}</FriendOnlineStatus>
 
-}
-
-FriendListItem.propTypes = {
-    friend: PropTypes.shape({
-          avatar: PropTypes.string.isRequired,
-          name: PropTypes.string.isRequired,
-          isOnline: PropTypes.bool.isRequired,
-          id: PropTypes.number.isRequired,
-        }).isRequired
-  };
+      <img src={avatar} alt="User avatar" width="48" />
+      <FriendName>{name}</FriendName>
+    </FriendItem>
+  );
+};
